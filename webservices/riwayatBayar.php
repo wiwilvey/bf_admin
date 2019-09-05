0ac8f8ad0f2e33eeccc6f69142a36a9daf1824e0
wiwilvey <wilveywhye@gmail.com>
2019-09-05 18:47:04 +0700

    riwayat bayaran

---
+++ webservices/riwayatBayar.php
@@ -0,0 +1,16 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+
+require "../kelas/class.db.php";
+$wkl = new db();
+$conn = $wkl->koneksi();
+
+$sql = "SELECT DATE_FORMAT(`TANGGAL`,'d%-m%-y%') TANGGAL,`tbpemasukan`.`KODE_POST`,`tbpostanggaran`.`NAMA_POSTING` , `JUMLAH` FROM `tbpemasukan` , `tbpostanggaran` WHERE `NIS`= {$_GET['nis']} && `tbpostanggaran`.`KODE_BIAYA` = `tbpemasukan`.`KODE_POST` ORDER BY `tbpemasukan`.`TANGGAL`";
+// echo $sql;
+$qry = $conn->query($sql);
+$data = [];
+while( $res = $qry->fetch_assoc()){
+    array_push($data,$res);
+}
+
+echo json_encode($data);
