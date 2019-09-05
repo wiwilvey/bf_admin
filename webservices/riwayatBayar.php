52f182075470cf918eebcf5887202b26bc50427f
wiwilvey <wilveywhye@gmail.com>
2019-09-05 19:50:42 +0700

    mix riwayat resume

--- webservices/riwayatBayar.php
+++ webservices/riwayatBayar.php
@@ -8,9 +8,17 @@ $conn = $wkl->koneksi();
 $sql = "SELECT DATE_FORMAT(`TANGGAL`,'%d-%m-%Y') TANGGAL,`tbpemasukan`.`KODE_POST`,`tbpostanggaran`.`NAMA_POSTING` , `JUMLAH` FROM `tbpemasukan` , `tbpostanggaran` WHERE `NIS`= {$_GET['nis']} && `tbpostanggaran`.`KODE_BIAYA` = `tbpemasukan`.`KODE_POST` ORDER BY `tbpemasukan`.`TANGGAL`";
 // echo $sql;
 $qry = $conn->query($sql);
-$data = [];
+$datariwayat = [];
 while( $res = $qry->fetch_assoc()){
-    array_push($data,$res);
+    array_push($datariwayat,$res);
 }
 
-echo json_encode($data);
+$sql = "SELECT NAMA_POSTING , SUM(JUMLAH) JUMLAH FROM tbpemasukan , tbpostanggaran WHERE NIS = '{$_GET['nis']}' && tbpostanggaran.KODE_BIAYA = tbpemasukan.KODE_POST GROUP BY KODE_POST";
+$qry = $conn->query($sql);
+$dataresume = [];
+while( $res = $qry->fetch_assoc()){
+    array_push($dataresume,$res);
+}
+
+$output = ['riwayat'=>$datariwayat , 'resume'=>$dataresume];
+echo json_encode($output);
